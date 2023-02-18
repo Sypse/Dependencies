91bbea4019ae3305ae67c76fe9cfff57f0a8f318
Sypse <71714559+Sypse@users.noreply.github.com>
2023-02-18 23:17:16 +0100

    Update customtween.lua
    simpler

--- customtween.lua
+++ customtween.lua
@@ -1,5 +1,5 @@
 local twn = {};
-function twn:customtwn(input, studspersecond, offset)
+function twn(input, studspersecond, offset)
    local char = game:GetService("Players").LocalPlayer.Character;
    local input = input or error("input is nil");
    local studspersecond = studspersecond or 1000;
